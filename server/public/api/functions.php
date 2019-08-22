d4ba0545942d73eb63f16c7bdfbb73dc373f3630
Mona-S <forward2cm@gmail.com>
2019-08-22 18:35:19 +0000

    Encode the output variable into JSON, and put the resulting string in json_output and print out the json_output

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -4,8 +4,10 @@ function error_handler($error) {
     $output = array(
         "Sucesss" => false,
         "error" => $error -> getMessage()
-
     );
+
 }
+$json_output = json_encode($output);
+print($json_output);
 
 ?>
