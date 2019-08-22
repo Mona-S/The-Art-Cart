13e4819b2a17f6f4b3fa8a5706050b1164900ac6
Mona-S <forward2cm@gmail.com>
2019-08-22 23:18:08 +0000

    removed commented out code and moved status code in error handler

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -5,6 +5,8 @@ function error_handler($error) {
         "Sucesss" => "false",
         "error" => $error -> getMessage(),
     );
+    
+    http_response_code(500);
 
     $json_output = json_encode($output);
     print($json_output);
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -6,18 +6,9 @@ require_once('./functions.php');
 
 set_exception_handler('error_handler');
 
-// http_response_code(500);
-// doStuff();
+header('Content-Type: application/json');
 
 $output = file_get_contents('./dummy-products-list.json');
 print($output);
-
-// header('Content-Type: application/json');
-
-// if (empty($_GET['id'])) {
-//   readfile('dummy-products-list.json');
-// } else {
-//   readfile('dummy-product-details.json');
-// }
 
 ?>
