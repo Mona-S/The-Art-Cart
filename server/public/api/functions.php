d89e532a6c72ca18ea1d2101ba4cdc9bd9311341
Mona-S <forward2cm@gmail.com>
2019-08-23 01:38:44 +0000

    connected to products table and fetched data

--- server/public/api/functions.php
+++ server/public/api/functions.php
@@ -5,11 +5,15 @@ function error_handler($error) {
         "Sucesss" => "false",
         "error" => $error -> getMessage(),
     );
-    
+
     http_response_code(500);
 
     $json_output = json_encode($output);
     print($json_output);
+}
+
+function startup() {
+    header('Content-Type: application/json');
 }
 
 
--- server/public/api/products.php
+++ server/public/api/products.php
@@ -4,10 +4,24 @@ require_once('./functions.php');
 set_exception_handler('error_handler');
 set_error_handler('error_handler');
 
+startup();
+
 require_once('./db_connection.php');
 header('Content-Type: application/json');
 
-$output = file_get_contents('./dummy-products-list.json');
-print($output);
+$query = "SELECT * FROM `products`";
+$result = mysqli_query($conn, $query);
+
+if(!$result){
+  throw new Exception(mysqli_connect_error());
+}
+
+$output = array("Success" => true,
+"data"=> []);
+
+while ($row = mysqli_fetch_assoc($result)) {
+  $data[] = $row;
+};
+print(json_encode($data));
 
 ?>
