1791bb8637e129a28c2429a230fc77bc4ba4f20f
Mona-S <forward2cm@gmail.com>
2019-08-23 00:37:39 +0000

    created connection file for mysql

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -1,11 +1,10 @@
 <?php
 
-use PHPMailer\PHPMailer\Exception;
-
 require_once('./functions.php');
-
 set_exception_handler('error_handler');
+set_error_handler('error_handler');
 
+require_once('./db_connection.php');
 header('Content-Type: application/json');
 
 $output = file_get_contents('./dummy-products-list.json');
