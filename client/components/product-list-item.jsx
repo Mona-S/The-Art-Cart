81e528faf05d76ea2640c015fb34c5a7dbcf7693
Mona-S <forward2cm@gmail.com>
2019-08-23 21:54:48 +0000

    In progress Modify ProductDetails's componentDidMount method to request product details based on the props it now receives from App

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -7,7 +7,7 @@ function ProductListItem(props) {
       <div className="card-body">
         <img src={props.image} alt="product1" className="card-img"/>
         <h4 className="card-title">{props.name}</h4>
-        <p className="card-text">${props.price}</p>
+        <p className="card-text">${(props.price / 100).toFixed(2)}</p>
         <p className="card-text">{props.shortDescription}</p>
       </div>
     </div>
