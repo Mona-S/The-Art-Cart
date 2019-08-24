c2f2a1173d3b04c5aa4ada35960216b1277900c8
Mona-S <forward2cm@gmail.com>
2019-08-24 23:58:39 +0000

    checked if there is an ID but no data, throw an exception of invalid id

--- server/public/api/products.php
+++ server/public/api/products.php
@@ -13,6 +13,7 @@ if(empty($_GET['id'])){
 } 
 else {
   $whereClause = "WHERE id = " . $_GET['id'];
+  $id = $_GET['id'];
 
   if(!is_numeric($_GET['id'] )){
    throw new Exception('id needs to be a number');
@@ -21,6 +22,10 @@ else {
 
 $query = "SELECT * FROM `products`" .$whereClause;
 $result = mysqli_query($conn, $query);
+
+if(!mysqli_num_rows($result)){
+  throw new Exception('Invalid ID: '.$id);
+}
 
 if(!$result){
   throw new Exception(mysqli_connect_error());
