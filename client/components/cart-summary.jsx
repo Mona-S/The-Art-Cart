e0a693960f7fcd452dc7b4ffd31bdb606427b20f
Mona-S <forward2cm@gmail.com>
2019-08-28 20:02:35 +0000

    Define a stateless CartSummary component that takes an Array of cart items via props and renders a list of CartSummaryItems. CartSummary should also have a heading for the list and display the total price of all cart items. If there are no items in the cart, then CartSummary should display a message to the user

--- client/components/cart-summary.jsx
+++ client/components/cart-summary.jsx
@@ -2,25 +2,39 @@ import React from 'react';
 import CartSummaryItem from './cart-summary-item';
 
 function CartSummary(props) {
-  // const cartArray = props.cartItems;
+  if (props.totalItems === 0) {
+    return (
+      <React.Fragment>Cart is Empty</React.Fragment>
+    );
+  }
+  const items = props.totalItems.map((item, index) => {
+    return (
+      <CartSummaryItem key={index}
+        image= {item.image}
+        name = {item.name}
+        price = {item.price}
+        shortDescription={item.shortDescription}/>
+    );
+  });
 
+  const total = cartTotalPrice(props.totalItems);
   return (
-    <React.Fragment>
-      <h3>Total Price</h3>
-      <CartSummaryItem></CartSummaryItem>
-    </React.Fragment>
+    <div className= "container">
+      <button type="button" className="btn btn-outline-info"
+        onClick={() => this.props.productView('catalog', {})}>Back to Catalog</button>
+      <h3>My Cart</h3>
+      <div>{items}</div>
+      <p>$ Total Price {(total / 100).toFixed(2)}</p>
+    </div>
   );
 }
 
-export default CartSummary;
+function cartTotalPrice(cartItems) {
+  let totalPrice = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    totalPrice += cartItems[i].price;
+  }
+  return totalPrice;
+}
 
-// getCartTotal(myArray);
-//   return getCartTotal;
-//   }
-// function getCartTotal(cartItems) {
-//   var total = 0;
-//   for (var i = 0; i < cartItems.length; i++){
-//     total += carItems[i].price;
-//   }
-//   return total;
-// }
+export default CartSummary;
