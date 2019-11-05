17d998b4a6a55ff040af771bb025ebf980d849e9
Mona-S <forward2cm@gmail.com>
2019-11-05 05:11:53 +0000

    sql query for insert into cartitems

--- server/public/api/cart_add.php
+++ server/public/api/cart_add.php
@@ -55,6 +55,24 @@ if(!$cartID){
     $_SESSION ['cartId'] = $cartID;
 }
 
+$productData['productID'] = $id;
+$productData['cartID'] = $cartID;
+
+$cartItemsInsert = "INSERT INTO cartItems(productID, count, added, cartID)
+VALUES (". $id .", 1, NOW(), ". $cartID . ") ON DUPLICATE KEY UPDATE 
+count = count + 1";
+
+$fullResult = mysqli_query($conn, $cartItemsInsert);
+$count = 1;
+
+if(mysqli_affected_rows($conn) < 1){
+    mysqli_query($conn, "ROLLBACK");
+    throw new Exception('Something went wrong');
+}
+
+$commit = mysqli_query($conn, "COMMIT");
+print(json_encode($productData));
+
 
 
 
